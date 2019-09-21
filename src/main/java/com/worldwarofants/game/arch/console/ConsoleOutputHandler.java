ddb7f1792649de87e302d464740eec685172fadb
AdamPhilipSmith <adamphilipsmith@hotmail.co.uk>
2019-09-21 10:25:28 +0100

    My attempt at making a fluid interface. Seems to work but I've had to do some work around that I'm sure aren't correct.

--- src/main/java/com/worldwarofants/game/arch/console/ConsoleOutputHandler.java
+++ src/main/java/com/worldwarofants/game/arch/console/ConsoleOutputHandler.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 public class ConsoleOutputHandler{
 
     private static final long SPELL_DELAY = 15;
+    private Object ConsoleOutputHandler;
 
     private void ConsoleOutputHandler(){
         //this class is intended to be static and not instanced.
@@ -13,7 +14,13 @@ public class ConsoleOutputHandler{
     /**
      * output the provided string in one line, character by character
      */
-    public static void spell(String input) {
+
+    public ConsoleOutputHandler newOutput(){
+        ConsoleOutputHandler = new ConsoleOutputHandler();
+        return this;
+
+    }
+    public ConsoleOutputHandler spell(String input) {
         for(int i = 0; i < input.length(); i++) {
             System.out.print(input.charAt(i));
             try {
@@ -24,17 +31,28 @@ public class ConsoleOutputHandler{
                 Thread.currentThread().interrupt();
             }
         }
+        return this;
     }
     /**
      * output the provided string in one line
      */
-    public static void post(String input) {
-        System.out.println(input);   
+    public ConsoleOutputHandler post(String input) {
+        System.out.println(input);
+        return this;
     }
     /**
      * output a line break
      */ 
-    public static void lineBreak() {
-        System.out.println(); 
+    public ConsoleOutputHandler lineBreak() {
+        System.out.println();
+        return this;
+    }
+
+    public void print(){
+        try {
+            finalize();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
     }
 }
