0d854b064e3109f9ddee0b28b9682a954141d2cf
liam027 <45797945+liam027@users.noreply.github.com>
2019-09-22 06:18:34 -0400

    TutorialMenu make descriptions static

--- src/main/java/com/worldwarofants/game/module/tutorial/TutorialView.java
+++ src/main/java/com/worldwarofants/game/module/tutorial/TutorialView.java
@@ -5,12 +5,12 @@ import com.worldwarofants.game.arch.console.ConsoleOutputHandler;
 
 public class TutorialView extends AbstractView<TutorialViewModel> {
 
-	private final String START_MESSAGE = "Your colony is under attack! You are on the brink of defeat and you must fall back in order to avoid complete annihilation! ...";
-	private final String FIGHT_MESSAGE = "Your entire colony has been overrun and annihilated; a colony with such a low POPULATION is no match for an enemy of this size!";
-	private final String RETREAT_MESSAGE = "You made the right decision!";
-	private final String FIGHT_DESC = "Fight";
-	private final String RETREAT_DESC = "Retreat";
-	private final String QUIT_DESC = "Quit";
+	private static final String START_MESSAGE = "Your colony is under attack! You are on the brink of defeat and you must fall back in order to avoid complete annihilation! ...";
+	private static final String FIGHT_MESSAGE = "Your entire colony has been overrun and annihilated; a colony with such a low POPULATION is no match for an enemy of this size!";
+	private static final String RETREAT_MESSAGE = "You made the right decision!";
+	private static final String FIGHT_DESC = "Fight";
+	private static final String RETREAT_DESC = "Retreat";
+	private static final String QUIT_DESC = "Quit";
 	
 	public TutorialView(TutorialViewModel viewModel) {
 		super(viewModel);
