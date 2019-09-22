e061dc4d60071b54dd9ea7488f85f13160fc2a86
liam027 <45797945+liam027@users.noreply.github.com>
2019-09-22 06:10:32 -0400

    NewGameView: make command descriptions view clas properties

--- src/main/java/com/worldwarofants/game/module/newgame/NewGameView.java
+++ src/main/java/com/worldwarofants/game/module/newgame/NewGameView.java
@@ -5,14 +5,15 @@ import com.worldwarofants.game.arch.console.ConsoleOutputHandler;
 
 public class NewGameView extends AbstractView<NewGameViewModel> {
 
+	final String START_GAME_DESC = "Start Game";
+	final String BACK_DESC = "Back";
+	final String TITLE = "GAME SETUP";
+	
 	public NewGameView(NewGameViewModel viewModel) {
 		super(viewModel);
 	}
 
 	public void renderNewGameScreen() {
-		final String START_GAME_DESC = "Start Game";
-		final String BACK_DESC = "Back";
-		final String TITLE = "GAME SETUP";
 		ConsoleOutputHandler.lineBreak();
 		ConsoleOutputHandler.spellTitle(TITLE);
 		ConsoleOutputHandler.lineBreak();
